b39558f4bf7bfaa14e76bb458049224e5c3547a0
Castrokimaru <castrokimaru@gmail.com>
2025-12-07 11:30:41 +0300

    sesseion maker

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///lib/turf_time.db')
+engine = create_engine('sqlite:///turf_time.db')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
